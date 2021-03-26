e2c70c65658a0c3d146489dc9c91e6a7ea740348
HeimdallCode <jefersom.siqueira@hotmail.com>
2021-03-26 12:16:07 -0300

    add

--- src/L02_EX18.py
+++ src/L02_EX18.py
@@ -1,41 +1,41 @@
 # -*- coding: utf-8 -*-
 
-"""Resolução Lista 02 Exercicio 17 Python Brasil (J.Siqueira 03/21)."""
+"""Resolução Lista 02 Exercicio 18 Python Brasil (J.Siqueira 03/21)."""
 
-# Faça um Programa que leia um número inteiro menor que 1000 e imprima a
-# quantidade de centenas, dezenas e unidades do mesmo. Observando os termos
-# no plural a colocação do "e", da vírgula entre outros.
-# Exemplo:326 = 3 centenas, 2 dezenas e 6 unidades
-#               12 = 1 dezena e 2 unidades
-#
-# Testar com: 326, 300, 100, 320, 310,305, 301, 101, 311, 111, 25, 20, 10, 21,
-#             11, 1, 7 e 16
+# Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a
+# mesma é uma data válida.
+# JAN  01   31
+# FEV  02   28
+# MAR  03   31
+# ABR  04   30
+# MAI  05   31
+# JUN  06   30
+# JUL  07   31
+# AGO  08   30
+# SET  09   31
+# OUT  10   30
+# NOV  11   31
+# DEZ  12   30
 
+d = input('informe uma data: ').split('/')
 
-n = str(input("\nDigite numero entre 0 - 999: "))
+dia = d[0]
+mes = d[1]
+ano = d[2]
 
-a = len(n)
-c_s = 's'
-d_s = 's'
-u_s = 's'
+va = False
+vm = False
 
-if a == 3:
-    if n[0] == '1':
-        c_s = ''
-        if n[1] == '1':
-            d_s = ''
-            if n[2] == '1':
-                u_s = ''
-    print(f'{n[0]} centena{c_s}, {n[1]} dezena{d_s} e {n[2]} unidade{u_s}.')
-elif a == 2:
-    if n[0] == '1':
-        d_s = ''
-        if n[1] == '1':
-            u_s = ''
-    print(f'{n[0]} dezena{d_s} e {n[1]} unidade{u_s}.')
-elif a == 1:
-    if n[0] == '1':
-        u_s = ''
-    print(f'{n[0]} unidade{u_s}.')
+if len(ano) <= 4 and int(ano) >= 1900:
+    va = True
+    if mes == '02' and int(dia) <= 28:
+        vm = True
+    elif mes in '010305070911' and int(dia) <= 31:
+        vm = True
+    elif mes in '0406081012' and int(dia) <= 30:
+        vm = True
+
+if va is True and vm is True:
+    print(f'\n\033[1;33mA data {"/".join(d)} é válida\033[1;m')
 else:
-    (print('Valor inválido!'))
+    print(f'\n\033[1;31mA data {"/".join(d)} é inválida\033[1;m')
