0085b9a9f4a34a517d477c84fea1f12eea362a04
HeimdallCode <jefersom.siqueira@hotmail.com>
2021-03-29 20:46:42 -0300

    add

--- src/L02_EX25.py
+++ src/L02_EX25.py
@@ -14,3 +14,29 @@
 # pessoa no crime. Se a pessoa responder positivamente a 2 questões ela deve
 # ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como
 # "Assassino". Caso contrário, ele será classificado como "Inocente".
+
+p1 = input('Telefonou para vítima?....: ')
+p2 = input('Esteve no local do crime?.: ')
+p3 = input('Mora perto da vítima?.....: ')
+p4 = input('Devia para vítima?........: ')
+p5 = input('Já trabalhou com a vítima?: ')
+
+r = 0
+
+if p1 in 'sS':
+    r += 1
+if p2 in 'sS':
+    r += 1
+if p3 in 'sS':
+    r += 1
+if p4 in 'sS':
+    r += 1
+if p5 in 'sS':
+    r += 1
+
+if r <= 2:
+    print('\nSuspeita')
+if r >= 3 and r <= 4:
+    print('\nCúmplice')
+if r >= 5:
+    print('\nAssassino')
