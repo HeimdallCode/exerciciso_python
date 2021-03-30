eaac0a7b3dddfad1bf609aa2a3db64e227607cab
HeimdallCode <jefersom.siqueira@hotmail.com>
2021-03-30 09:02:53 -0300

    add

--- src/L02_EX27.py
+++ src/L02_EX27.py
@@ -11,3 +11,31 @@
 # ultrapassar R$ 25,00, receberá ainda um desconto de 10% sobre este total.
 # Escreva um algoritmo para ler a quantidade (em Kg) de morangos e a quantidade
 # (em Kg) de maças adquiridas e escreva o valor a ser pago pelo cliente.
+
+f1 = float(input('Quantidade em [kg] de maça...: '))
+f2 = float(input('Quantidade em [kg] de morango: '))
+
+f1_preco = 0
+f2_preco = 0
+desconto = 0
+
+if f1 <= 5:
+    f1_preco = f1 * 2.5
+else:
+    f1_preco = f1 * 2.2
+
+if f2 <= 5:
+    f2_preco = f2 * 1.8
+else:
+    f2_preco = f2 * 1.5
+
+if (f1 + f2) >= 8 or (f1_preco + f2_preco) >= 25:
+    desconto = (f1_preco + f2_preco) * 0.01
+
+print('\n* * * Cupom Fiscal * * *\n')
+print(f'Morangos\t{f1}kg\t\t{f1_preco:.2f}')
+print(f'Maças\t\t{f2}kg\t\t{f2_preco:.2f}')
+print('-----------------------------------------')
+print(f'Valor total\t\t\t{f1_preco + f2_preco:.2f}')
+print(f'Desconto 10%\t\t\t {desconto:.2f}')
+print(f'Valor a pagar\t\t\t{((f1_preco + f2_preco) - desconto):.2f}')
