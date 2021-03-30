61ddb7cb2f539e21d5071390d7955dda48f6fbab
HeimdallCode <jefersom.siqueira@hotmail.com>
2021-03-30 10:39:26 -0300

    add

--- src/L02_EX28.py
+++ src/L02_EX28.py
@@ -16,3 +16,48 @@
 # tipo e a quantidade de carne comprada pelo usuário e gere um cupom fiscal,
 # contendo as informações da compra: tipo e quantidade de carne, preço total,
 # tipo de pagamento, valor do desconto e valor a pagar.
+
+
+quantidade = float(input('Informe quantos [kg] de carne...............: '))
+tipo_produto = input('Qual tipo [F]ile, [A]lcatra, [P]icanha......: ')
+tipo_pagamento = input('Qual forma de pagamento [D]inheiro, [C]artão: ')
+
+desconto = 0
+valor = 0
+
+if tipo_produto in 'fF':
+    tipo_produto = 'File duplo'
+    if quantidade <= 5:
+        valor = 5.9 * quantidade
+    else:
+        valor = 6.8 * quantidade
+elif tipo_produto in 'aA':
+    tipo_produto = 'Alcatra'
+    if quantidade <= 5:
+        valor = 4.9 * quantidade
+    else:
+        valor = 5.8 * quantidade
+elif tipo_produto in 'pP':
+    tipo_produto = 'Picanha'
+    if quantidade <= 5:
+        valor = 6.9 * quantidade
+    else:
+        valor = 7.8 * quantidade
+else:
+    print('\n\033[1;31mValor inválido!\033[1;m')
+
+if tipo_pagamento in 'cC':
+    tipo_pagamento = 'Cartão'
+    desconto = 0.05 * valor
+else:
+    tipo_pagamento = 'Dinheiro'
+    desconto = 0
+
+if quantidade >= 0:
+    print('\n* * * Cupom Fiscal * * *\n')
+    print(f'{tipo_produto}\t\t\t{quantidade:.1f}kg')
+    print('-----------------------------------------')
+    print(f'Valor total\t\t\t{valor:.2f}')
+    print(f'Forma de pagamento\t\t{tipo_pagamento}')
+    print(f'Desconto 5%\t\t\t{desconto:.2f}')
+    print(f'Valor a pagar\t\t\t{((valor) - desconto):.2f}')
