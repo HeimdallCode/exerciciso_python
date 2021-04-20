f70e09381406ce32244e94ac7c082e4000da090e
HeimdallCode <jefersom.siqueira@hotmail.com>
2021-04-20 12:40:36 -0300

    add

---
+++ src/fstring_exemplos.py
@@ -0,0 +1,14 @@
+"""Estudo sobre Fstring e funções."""
+
+NOME = "Jefersom"
+SOBRENOME = "Siqueira"
+VALOR = 98
+VALOR2 = 1875
+VALOR3 = 18.87
+VALOR4 = 118.83
+x = 10
+print(f"{NOME:*^50}")
+print(f"{VALOR:0>{x}}")
+print(f"{VALOR2:0>{x}}")
+print(f"{VALOR3:0>{x}}")
+print(f"{VALOR4:0>{x}}")
