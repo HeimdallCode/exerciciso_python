169a5d4352b64dd8a09862d7186a9091577a09b9
HeimdallCode <jefersom.siqueira@hotmail.com>
2021-05-21 13:23:03 -0300

    add

--- src/fstring_exemplos.py
+++ src/fstring_exemplos.py
@@ -6,9 +6,9 @@ VALOR = 98
 VALOR2 = 1875
 VALOR3 = 18.87
 VALOR4 = 118.83
-x = 10
-print(f"{NOME:*^50}")
-print(f"{VALOR:0>{x}}")
-print(f"{VALOR2:0>{x}}")
-print(f"{VALOR3:0>{x}}")
-print(f"{VALOR4:0>{x}}")
+X = 6
+print(f"\n{NOME:*^50}")
+print(f"{VALOR:0>{X}}")
+print(f"{VALOR2:0>{X}}")
+print(f"{VALOR3:0>{X}}")
+print(f"{VALOR4:0>{X}}")
