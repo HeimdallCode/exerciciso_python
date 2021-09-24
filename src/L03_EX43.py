0aa172da4499112f6355fa8ea31b7674ff502e1b
HeimdallCode <jefersom.siqueira@hotmail.com>
2021-09-24 18:59:50 -0300

    -

---
+++ src/L03_EX43.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""Resolução Lista 3 Exercicio 43 Python Brasil (J.Siqueira 03/21)."""
+
+# O cardápio de uma lanchonete é o seguinte:
+
+# Especificação     Código      Preço
+# Cachorro Quente   100         1,20
+# Bauru Simples     101         1,30
+# Bauru com ovo     102         1,50
+# Hambúrguer        103         1,30
+# Cheeseburguer     104         1,20
+# Refrigerante      105         1,00
+
+# Faça um programa que leia o código dos itens pedidos e as quantidades 
+# desejadas. Calcule e mostre o valor a ser pago por item (preço * quantidade)
+# e o total geral do pedido. Considere que o cliente deve informar quando o 
+# pedido deve ser encerrado.
+
